de877f9a8b00f08899da0d08700fab96dd6b1a9b
roh210 <roheena.s786@gmail.com>
2024-09-02 13:24:13 +0100

    added rightside bar

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -1,8 +1,74 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import RenderTag from "./RenderTag";
+
+const hotQuestions = [
+  {
+    _id: 1,
+    title:
+      "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
+  },
+  { _id: 2, title: "How to create a custom hook in React?" },
+  { _id: 3, title: "How to use React Context API?" },
+  { _id: 4, title: "How to use React Router?" },
+  { _id: 5, title: "How to use React Hooks?" },
+];
+
+const popularTags = [
+  { _id: 1, name: "Next JS", totalQuestions: 32 },
+  { _id: 2, name: "React", totalQuestions: 10 },
+  { _id: 3, name: "React Native", totalQuestions: 34 },
+  { _id: 4, name: "React Hooks", totalQuestions: 60 },
+  { _id: 5, name: "React Context API", totalQuestions: 2 },
+];
 
 const RightSideBar = () => {
   return (
-    <section className="custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-fit flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-md:hidden lg:w-[266px]"></section>
+    <section className="custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-fit flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden lg:w-[350px]">
+      <div>
+        <h3 className="h3-bold text-dark300_light900">Top Questions</h3>
+        <div className="mt-7 flex w-full flex-col gap-[30px]">
+          {hotQuestions.map((question) => {
+            return (
+              <Link
+                key={question._id}
+                href={`/questions/${question._id}`}
+                className="flex cursor-pointer items-center justify-between gap-7"
+              >
+                <p className="body-medium text-dark200_light900">
+                  {question.title}
+                </p>
+                <Image
+                  src="/assets/icons/chevron-right.svg"
+                  alt="arrow-right"
+                  width={20}
+                  height={20}
+                  className="invert-colors"
+                />
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="mt-16">
+        <h3 className="h3-bold text-dark300_light900">Popular Tags</h3>
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map((tag) => {
+            return (
+              <RenderTag
+                key={tag._id}
+                _id={tag._id}
+                name={tag.name}
+                totalQuestions={tag.totalQuestions}
+                showCount
+              />
+            );
+          })}
+        </div>
+      </div>
+    </section>
   );
 };
 
