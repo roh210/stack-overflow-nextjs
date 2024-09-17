42f2ef6b1b9693a3b1df23490398ed8d32246cd9
roh210 <roheena.s786@gmail.com>
2024-09-17 18:50:29 +0100

    add api webhook

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,35 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/ask-question(.*)"]);
+// Define matchers for public and ignored routes
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/webhook",
+  "/question/:id",
+  "/tags",
+  "/tags:id",
+  "/profile:id",
+  "/community",
+  "/jobs",
+]);
+
+const isIgnoredRoute = createRouteMatcher(["/api/webhook", "/api/chatgpt"]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  // If the route is ignored, skip the auth check
+  if (isIgnoredRoute(req)) {
+    return;
+  }
+
+  // If the route is public, allow access without authentication
+  if (isPublicRoute(req)) {
+    return;
+  }
+
+  // For any other route, require authentication
+  auth();
 });
 
+// Configure the matcher for which routes to apply the middleware
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
