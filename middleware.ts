016368eaf80f57355f3c2251c35f662437cd2eeb
roh210 <roheena.s786@gmail.com>
2024-09-18 14:39:49 +0100

    removed code commented out

--- middleware.ts
+++ middleware.ts
@@ -1,38 +1,3 @@
-/* import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-const publicRoutes = createRouteMatcher([
-  "/",
-  "/api/webhooks",
-  "/question/:id",
-  "/tags",
-  "/tags/:id",
-  "/profile/:id",
-  "/community",
-  "/jobs",
-]);
-
-const ignoredRoutes = createRouteMatcher(["/api/webhooks", "/api/chatgpt"]);
-
-export default clerkMiddleware((auth, req) => {
-  if (publicRoutes(req)) {
-    // Allow access to public routes without authentication
-    return;
-  }
-
-  if (ignoredRoutes(req)) {
-    // Skip Clerk middleware for ignored routes
-    return;
-  }
-
-  // For all other routes, you can decide whether to protect them or not
-  auth().protect();
-});
-
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
-  */
-
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const publicRoutes = createRouteMatcher([
