ed0a8df07ac84e6bc5a1d54e3eac9d0836b7ec9b
roh210 <roheena.s786@gmail.com>
2024-09-28 18:42:56 +0100

    change

--- components/card/UserCard.tsx
+++ components/card/UserCard.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import RenderTag from "../shared/RenderTag";
+// import RenderTag from "../shared/RenderTag";
 // import RenderTag from "../shared/RenderTag";
 
 // get user data from pages
