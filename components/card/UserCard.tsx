694c567ff5ae2d79579d0a14fcfc5c5a411e0a85
roh210 <roheena.s786@gmail.com>
2024-09-29 15:19:50 +0100

    completed community ui and some backend by creating tag action

--- components/card/UserCard.tsx
+++ components/card/UserCard.tsx
@@ -1,6 +1,9 @@
+import { getTopInteractedTags } from "@/lib/actions/tag.action";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Badge } from "../ui/badge";
+import RenderTag from "../shared/RenderTag";
 // import RenderTag from "../shared/RenderTag";
 // import RenderTag from "../shared/RenderTag";
 
@@ -15,19 +18,42 @@ interface UserCardProps {
   };
 }
 
-const UserCard = ({ user }: UserCardProps) => {
+const UserCard = async ({ user }: UserCardProps) => {
+  const interactedTags = await getTopInteractedTags({ userId: user._id });
   return (
     <>
-      <Link href={`/profile/:${user.clerkId}`}>
-        <Image
-          src={`/${user.picture}`}
-          alt={"user-pic"}
-          width={100}
-          height={100}
-          className="cursor-pointer rounded-full"
-        />
-        <h3 className="h3-bold text-dark200_light900 my-2.5">{user.name}</h3>
-        <p className="body-regular text-dark500_light500">@{user.username}</p>
+      <Link
+        href={`/profile/:${user.clerkId}`}
+        className="shadow-light100_darknone w-full max-xs:min-w-full xs:w-[260px]"
+      >
+        <article className="background-light900_dark200 light-border flex w-full flex-col items-center justify-center rounded-2xl border p-8">
+          <Image
+            src={user.picture}
+            alt={"user-pic"}
+            width={100}
+            height={100}
+            className="size-[100px] cursor-pointer rounded-full"
+          />
+          <div className="mt-4 text-center">
+            <h3 className="h3-bold text-dark200_light900 line-clamp-1">
+              {user.name}
+            </h3>
+            <p className="body-regular text-dark500_light500 mt-2">
+              @{user.username}
+            </p>
+          </div>
+          <div className="mt-5">
+            {interactedTags.length > 0 ? (
+              <div className="flex items-center gap-2">
+                {interactedTags.map((tag) => (
+                  <RenderTag key={tag._id} _id={tag.name} name={tag.name} />
+                ))}
+              </div>
+            ) : (
+              <Badge>No tags yet</Badge>
+            )}
+          </div>
+        </article>
       </Link>
     </>
   );
